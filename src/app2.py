4c2adcfea8ab5eff0f1985e08d07450ee1f941a9
Margoth <mloreana@gmail.com>
2021-04-10 19:22:41 +0000

    TD7

--- src/app2.py
+++ src/app2.py
@@ -1,16 +1,24 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, json
 app = Flask(__name__)
 
 todos = [{ "label": "Sample", "done": True }]
 
 # @app.route('/todos', methods=['GET'])
-# def hello_world():
-#     return '<h1>Hello!</h1>'
+# # def hello_world():
+# #     return '<h1>Hello!</h1>'
 
 @app.route('/todos', methods=['GET'])
 def hello_world():
     json_text = jsonify(todos)
     return json_text
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = request.data
+    request.data = json.loads(request_body)
+    json_text = jsonify(todos)
+    return json_text
+
 
 # These two lines should always be at the end of your app.py file.
 if __name__ == '__main__':
