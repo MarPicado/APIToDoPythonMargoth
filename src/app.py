fdf6abbefe71a7f4677f62c9088a1e9b66858ffc
Margoth <mloreana@gmail.com>
2021-04-10 22:41:27 +0000

    TD11

--- src/app.py
+++ src/app.py
@@ -23,6 +23,12 @@ def add_new_todo():
     print("Incoming request with the following body", request_body)
     return jsonify(todos)
 
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    print("This is the position to delete: ",position)
+    todos.pop(position)
+    return jsonify(todos)
+
 # These two lines should always be at the end of your app.py file.
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
